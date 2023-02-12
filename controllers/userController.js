b7908a019a9724a9fb0dbb2703f3dac1a2307b07
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-02-11 18:43:45 -0800

    Completes? userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,46 +20,76 @@ module.exports = {
                 : res.json(user)
         )
         .catch((err) => res.status(500).json(err));
-    }
+    },
 
     // POST/CREATE a new user
-
+    createUser(req, res) {
+        User.create(req.body)
+            .then((user) => res.json(user))
+            .catch((err) => res.status(500).json(err));
+    },
 
     // PUT/UPDATE a user by its _id
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true },
+        )
+            .then((user) => 
+            !user
+                ? res.status(404).json({ message: 'No user with that ID' })
+                : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
 
 
     // DELETE to remove user by its _id. BONUS: remove user and associated thoughts when deleted
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.userId })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with that ID' })
+                    : Thought.deleteMany({ _id: { $in: user.thoughts } })
+        )
+            .then(() => res.json({ message: 'User and associated thoughts deleted!' }))
+            .catch((err) => res.status(500).json(err));
+    },
+
+
 
 //////////////  -------  /api/users/:userId/friends/:friendId
 
     // POST to add a new friend to a user's friend list. ADD a friend
-
+    addFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.body } },
+            { runValidators: true, new: true }
+        )
+        .then((user) =>
+            !user
+                ? res.status(404).json({ message: 'No user with this id!' })
+                : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
 
     // DELETE to remove a friend from a user's friend list. REMOVE a friend
+    removeFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: { friendsId: req.params.friendsId } } },
+            { runValidators: true, new: true }
+        )
+        .then((user) =>
+            !user
+                ? res.status(404).json({ message: 'No user with this id!' })
+                : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+
+
 }
-
-
-
-
-
-getPosts(req, res) {
-    Post.find()
-      .then((posts) => res.json(posts))
-      .catch((err) => res.status(500).json(err));
-  },
-  getSinglePost(req, res) {
-    Post.findOne({ _id: req.params.postId })
-      .then((post) =>
-        !post
-          ? res.status(404).json({ message: 'No post with that ID' })
-          : res.json(post)
-      )
-      .catch((err) => res.status(500).json(err));
-  },
-  // create a new post
-  createPost(req, res) {
-    Post.create(req.body)
-      .then((dbPostData) => res.json(dbPostData))
-      .catch((err) => res.status(500).json(err));
-  },
-};
