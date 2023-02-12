8f76317459c48a6979553ac5381f0103858741a0
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-02-11 18:25:49 -0800

    Begins coding for getting users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,26 @@
 const { User, Thought } = require('../models');
 
-const userController = {
+module.exports = {
 
 //////////////  -------  /api/users
     // GET all users
-
+    getUsers(req, res) {
+        User.find()
+            .then((users) => res.json(users))
+            .catch((err) => res.status(500).json(err));
+    },
 
     // GET a single user by its _id and populated thought and friend data
-
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+        .select('-__v')
+        .then((user) => 
+            !user
+                ? res.status(404).json({ message: 'No user with that ID' })
+                : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+    }
 
     // POST/CREATE a new user
 
@@ -25,4 +38,28 @@ const userController = {
     // DELETE to remove a friend from a user's friend list. REMOVE a friend
 }
 
-module.exports = userController;
+
+
+
+
+getPosts(req, res) {
+    Post.find()
+      .then((posts) => res.json(posts))
+      .catch((err) => res.status(500).json(err));
+  },
+  getSinglePost(req, res) {
+    Post.findOne({ _id: req.params.postId })
+      .then((post) =>
+        !post
+          ? res.status(404).json({ message: 'No post with that ID' })
+          : res.json(post)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // create a new post
+  createPost(req, res) {
+    Post.create(req.body)
+      .then((dbPostData) => res.json(dbPostData))
+      .catch((err) => res.status(500).json(err));
+  },
+};
